b3b0f20d4fe7d525bb77516a8a3e5a53d719bbc8
Tyler Hall <thall49@vols.utk.edu>
2026-02-14 15:01:34 -0500

    Removing the gen_grid function, instead we store the result of the function on the grid

--- src/bifgraph.py
+++ src/bifgraph.py
@@ -7,21 +7,23 @@ class _FloatPairFn(Protocol):
 
 class BifGraph:
     def __init__(self, function: _FloatPairFn):
-        self._f = function
-        self._rmin =   -10
-        self._rmax =    10
-        self._xmin =   -10
-        self._xmax =    10
-        self._subs =  1000
-        self._grid =  None
-        self._title = None
+        self._f =  function
+        self._rmin   =  -10
+        self._rmax   =   10
+        self._xmin   =  -10
+        self._xmax   =   10
+        self._subs   = 1000
+        self._result = None
+        self._title  = None
 
-    def _gen_grid(self):
+    def _eval(self):
         r = np.linspace(self._rmin, self._rmax, self._subs, endpoint=True)
         x = np.linspace(self._xmin, self._xmax, self._subs, endpoint=True)
         R, X = np.meshgrid(r, x, indexing='xy')
 
-        self._grid = np.stack((R, X), axis=-1)
+        grid = np.stack((R, X), axis=-1)
+
+        self._result = np.apply_along_axis(self._f, axis=-1, arr=grid)
 
     def func(self, func: _FloatPairFn):
         self._f = func
