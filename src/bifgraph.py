13c7744d6e29abdb71c5bb9976e28ce584e9ceb7
Tyler Hall <thall49@vols.utk.edu>
2026-02-14 14:31:54 -0500

    Adding setter functions for the fields

--- src/bifgraph.py
+++ src/bifgraph.py
@@ -14,12 +14,28 @@ class BifGraph:
         self._xmax =    10
         self._subs =  1000
         self._grid =  None
+        self._title = None
 
     def _gen_grid(self):
         r = np.linspace(self._rmin, self._rmax, self._subs, endpoint=True)
         x = np.linspace(self._xmin, self._xmax, self._subs, endpoint=True)
+        R, X = np.meshgrid(r, x, indexing='xy')
 
-        X, Y = np.meshgrid(r, x, indexing='xy')
+        self._grid = np.stack((R, X), axis=1)
 
+    def func(self, func: _FloatPairFn):
+        self._f = func
 
-        self._grid = np.stack((X, Y), axis=1)
+    def title(self, title: str):
+        self._title = title
+
+    def rlim(self, tup: tuple[float, float]):
+        self._rmin(tup[0])
+        self._rmax(tup[1])
+
+    def xlim(self, tup: tuple[float, float]):
+        self._xmin(tup[0])
+        self._xmax(tup[1])
+
+    def subsections(self, num: int):
+        self._subs = num
