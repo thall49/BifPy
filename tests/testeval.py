f4d8fc142e5a449448a0ad2d74b9095c163f9333
Tyler Hall <thall49@vols.utk.edu>
2026-02-14 15:04:01 -0500

    Testing eval function

--- tests/testeval.py
+++ tests/testeval.py
@@ -2,7 +2,7 @@ from src.bifgraph import BifGraph
 import numpy as np
 
 def f(tup):
-    return tup[0] * tup[1]
+    return tup[0] + tup[1]
 
 bg = BifGraph(f)
 
@@ -10,7 +10,7 @@ bg.xlim((-1, 1))
 bg.rlim((-1, 1))
 bg.subsections(5)
 
-bg._gen_grid()
+bg._eval()
 
-print(bg._grid)
-print(bg._grid.shape)
+print(bg._result)
+print(bg._result.shape)
