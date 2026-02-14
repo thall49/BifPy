7610679572c39f7805a88a3f61b6174f746e2ea1
Tyler Hall <thall49@vols.utk.edu>
2026-02-14 15:41:18 -0500

    Claude fix this plz and ty

--- src/bifgraph.py
+++ src/bifgraph.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import brentq
 from typing import Protocol
 
 class _FloatPairFn(Protocol):
@@ -7,23 +8,56 @@ class _FloatPairFn(Protocol):
 
 class BifGraph:
     def __init__(self, function: _FloatPairFn):
-        self._f =  function
-        self._rmin   =  -10
-        self._rmax   =   10
-        self._xmin   =  -10
-        self._xmax   =   10
-        self._subs   = 1000
-        self._result = None
-        self._title  = None
+        self._f =   function
+        self._rmin   =   -10
+        self._rmax   =    10
+        self._xmin   =   -10
+        self._xmax   =    10
+        self._subs   =  1000
+        self._result =  None
+        self._grid   =  None
+        self._title  =  None
+        self._save   = False
+        self._name   =  None
+
+    def find_bif(self):
+        pos_roots = []
+        neg_roots = []
+        for r in np.linspace(self._rmin, self._rmax, self._subs, endpoint=True):
+            try:
+                x_pos = brentq(self._f, 0, self._xmax, args=(r,))
+                pos_roots.append((r, x_pos))
+            except ValueError:
+                pass
+
+            try:
+                x_neg = brentq(self._f, self._xmin, 0, args=(r,))
+                neg_roots.append((r, x_neg))
+            except ValueError:
+                pass
+        
+        pos_roots = np.array(pos_roots)
+        neg_roots = np.array(neg_roots)
+
+        plt.plot(pos_roots[:, 0], pos_roots[:, 1], 'r', label='pos')
+        plt.plot(neg_roots[:, 0], neg_roots[:, 1], 'b', label='neg')
+
+        plt.axvline(0, color='k', linestyle='--', alpha=0.5)
+        plt.xlabel('r')
+        plt.ylabel('x')
+        plt.title('Test')
+        plt.legend()
+        plt.show()
+
 
     def _eval(self):
         r = np.linspace(self._rmin, self._rmax, self._subs, endpoint=True)
         x = np.flip(np.linspace(self._xmin, self._xmax, self._subs, endpoint=True))
         R, X = np.meshgrid(r, x, indexing='xy')
 
-        grid = np.stack((R, X), axis=-1)
+        self._grid = np.stack((R, X), axis=-1)
 
-        self._result = np.apply_along_axis(self._f, axis=-1, arr=grid)
+        self._result = np.apply_along_axis(self._f, axis=-1, arr=self._grid)
 
     def func(self, func: _FloatPairFn):
         self._f = func
@@ -41,3 +75,7 @@ class BifGraph:
 
     def subsections(self, num: int):
         self._subs = num
+
+    def name(self, name: str):
+        self._show = True
+        self._name = name
--- tests/testeval.py
+++ tests/testeval.py
@@ -1,16 +1,9 @@
 from src.bifgraph import BifGraph
 import numpy as np
 
-def f(tup):
-    return tup[0] - tup[1]
+def f(r, x):
+    return x**2 - r
 
 bg = BifGraph(f)
 
-bg.xlim((-1, 1))
-bg.rlim((-1, 1))
-bg.subsections(5)
-
-bg._eval()
-
-print(bg._result)
-print(bg._result.shape)
+bg.find_bif()
